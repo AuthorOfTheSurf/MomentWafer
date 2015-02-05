a407dd12fdfeef543166eaabd67d628033aa4760
AuthorOfTheSurf <zanekansil@gmail.com>
2015-02-04 19:49:48 -0800

    Formatter parses BITalino header into pyDict

--- formatter.py
+++ formatter.py
@@ -5,14 +5,22 @@ Parses the json-text format into clean JSON more appropriate for the MATL applic
 """
 
 import sys
+import json
 
 def main():
 	try:
-		with open(sys.argv[1], "r") as bitalinoData:
-			data = bitalinoData.read()
-			print data
+		with open(sys.argv[1], "r") as BITfile:
+			data = BITfile.read()
+			header = headerToDict(data)
+			print json.dumps(header)
 	except IndexError as e:
 		print "Missing BITalino data file argument"
+
+"""
+Parses the json data in the BITalino header into a Python dict
+"""
+def headerToDict(BITdatastr):
+	return json.loads(BITdatastr.split("\n")[1][2:])
 
 if __name__ == '__main__':
     main()
