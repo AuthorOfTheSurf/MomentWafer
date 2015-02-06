6800ea5b5d38b01befc923230cee47fb68954496
AuthorOfTheSurf <zanekansil@gmail.com>
2015-02-05 21:30:41 -0800

    BITalinoData now fully-formed from bitalino src txt file

--- formatter.py
+++ formatter.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 """
 Used to format the raw data text files produced by a BITalino
-Parses the json-text format into clean JSON more appropriate for the MATL application
+Parses the json-text format into a Python object more appropriate for
+processing by the the MATL application
+
+Sample BITalino txt file:
+
+	# JSON Text File Format
+	# {"SamplingResolution": "10", "SampledChannels": [1], "SamplingFrequency": "1000", "ColumnLabels": ["SeqN", "Digital0", "Digital1", "Digital2", "Digital3", "EMG"], "AcquiringDevice": "98:D3:31:XX:XX:XX", "Version": "111", "StartDateTime": "2015-2-2 10:30:2"}
+	# EndOfHeader
+	0	1	1	1	1	467	
+	1	1	1	1	1	467	
+	2	1	1	1	1	472	
+	3	1	1	1	1	476	
+	4	1	1	1	1	478	
+	...
+
 """
 
 import sys
 import json
+import numpy
 
 class BITalinoData:
 	"""
 	Constructor
 	"""
 	def __init__(self, BITalinoTxtData=""):
-		self._dataStream = BITalinoTxtData.split("\n")
-		self._header = json.loads(self._dataStream[1][2:])
-		self._channels = {}
+		split = BITalinoTxtData.split("\r\n")
+		self._dataStream = split[3:]
+		self._header = json.loads(split[1][2:])
+		self._channels = self.makeChannels(self._dataStream)
 	"""
 	BITalino header as a Python dict
 		{
@@ -22,18 +38,14 @@ class BITalinoData:
 			"SampledChannels": [1],
 			"SamplingFrequency": "1000",
 			"ColumnLabels": ["SeqN", "Digital0", "Digital1", "Digital2", "Digital3", "EMG"],
-			"AcquiringDevice": "98:D3:31:B2:BD:4F",
+			"AcquiringDevice": "98:D3:31:XX:XX:XX",
 			"Version": "111",
 			"StartDateTime": "2015-2-2 10:30:2"
 		}
-
 	"""
 	@property
 	def header(self):
 	    return self._header
-	@header.setter
-	def header(self, value):
-	    self._header = value
 	"""
 	Raw multi-column data stream, array of tsv
 	The "Digital*" channels are not recording data in this instance
@@ -47,28 +59,48 @@ class BITalinoData:
 	@property
 	def dataStream(self):
 	    return self._dataStream
-	@dataStream.setter
-	def dataStream(self, value):
-	    self._dataStream = value
 	"""
 	List of the channels and their data
 		[
+			{ "label": "Digital0", "data": [1, 1, 1] },
+			...
 			{ "label": "EMG", "data": [270, 551, 432] }
 		]
 	"""
 	@property
 	def channels(self):
 	    return self._channels
-	@channels.setter
-	def channels(self, value):
-	    self._channels = value
+	"""
+	Parses the dataStream into and array of channel objects containing data and a label.
+	See the description for BITalinoData.channels
+	"""
+	def makeChannels(self, dataStream):
+		channels = []
+		labels = self._header["ColumnLabels"]
+		# Each line has a trailing tab, Datastream ends with a blank line
+		cleanStream = [line[:-1].split("\t") for line in dataStream[:-1]]
+		for col in range(len(labels)):
+			if col != 0:
+				# Convert to numpy.array then each column becomes the channel's data
+				# Also include label for the data
+				channels.append({
+					'label': labels[col],
+					'data': numpy.array(cleanStream)[:,col]
+				})
+		return channels
+	"""
+	This is up next, returns this object as pure JSON
+	Main use will be piping this output into a `.json` file
+	"""
+	def toJson(self, pretty):
+		pass
 
-	
 def main():
 	try:
 		with open(sys.argv[1], "r") as bitsrc:
 			data = BITalinoData(bitsrc.read())
-			print json.dumps(data.header)
+			print data.header
+			print data.channels
 	except IndexError as e:
 		print "Missing BITalino data file argument"
 
