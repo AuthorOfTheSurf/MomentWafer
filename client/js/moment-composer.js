2abddae38a32ca5e5fcc165161810a81934499b8
AuthorOfTheSurf <zanekansil@gmail.com>
2015-02-20 23:56:35 -0800

    Add frame-by-frame functionality to MC

--- client/js/moment-composer.js
+++ client/js/moment-composer.js
@@ -1,25 +1,30 @@
 (function() {
 
 // Knockout view model for MomentComposer
+//
 function MomentComposerViewModel() {
   var self = this;
   var $pop = Popcorn("#video");
-  // self.refresh() triggers computeds that call
-  // self.forceEval() to refresh their values
-  self.forceEval = ko.observable();
+
+  // Keep computed's up to date via refresh
+  // seed: call inside computed
+  // refresh: refresh all computed's that call seed
+  //
+  self.seed = ko.observable();
   self.refresh = function() {
-    self.forceEval(Math.random());
+    self.seed(Math.random());
   }
 
+  // Play button
+  //
   self.playButtonLabel = ko.computed(function() {
-    self.forceEval();
+    self.seed();
     return $pop.paused() ? "Play" : "Pause";
   })
   self.playButtonClass = ko.computed(function() {
-    self.forceEval();
+    self.seed();
     return $pop.paused() ? "button-primary" : "";
   })
-
   self.togglePlay = function() {
     if ($pop.paused()) {
       $pop.play();
@@ -30,10 +35,35 @@ function MomentComposerViewModel() {
     }
   }
 
+  // Frame-by-frame
+  //
+  var FRAME_RATES = [24, 30, 60, 120]
+  self.frameByFrameRates = ko.observableArray(FRAME_RATES);
+  self.selectedFrameRate = ko.observable();
+  self.frameStep = ko.computed(function() {
+    return 1.0 / self.selectedFrameRate();
+  });
+  self.selectedFrameRate.subscribe(function(newValue) {
+    console.log(newValue, self.frameStep());
+  });
+  self.previousFrame = function() {
+    $pop.pause();
+    $pop.currentTime($pop.currentTime() - self.frameStep());
+    self.refresh();
+  }
+   self.nextFrame = function() {
+    $pop.pause();
+    $pop.currentTime($pop.currentTime() + self.frameStep());
+    self.refresh();
+  }
+
+  // Utils
+  //
   self.printTime = function() {
     console.log($pop.currentTime());
   }
 }
+
 
 // Apply view model to view
 ko.applyBindings(new MomentComposerViewModel());
